e1c7be8e4e88788709b5f05c7458ffc2255e6312
Karthikeyan J <93634120+HoggyHog@users.noreply.github.com>
2022-07-27 15:17:22 +0530

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//const string path_dict = "../examples/google-books-common-words-extended.txt";
+const string path_file = "test-2-in.txt";
+//const string path_file = "../examples/test-1-in.txt";
+const string path_dict = "dictionary.txt";
+char freq_single[4] = {'E', 'T', 'A', 'O'};
